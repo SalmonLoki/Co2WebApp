b5040a64cb8f7cdeaec2b0c4d29934355e031bf9
SalmonLoki <kseniiakrovlya@gmail.com>
2019-05-22 17:57:35 +0300

    result

--- Co2WebApp/Models/Result.cs
+++ Co2WebApp/Models/Result.cs
@@ -1,5 +1,14 @@
 namespace Co2WebApp.Models {
 	public class Result {
+		public string type { get; }
+		public double value { get; }
 		
+		public string heartbeat { get; set; }
+
+		public Result(string type, double value, string heartbeat) {
+			this.type = type;
+			this.value = value;
+			this.heartbeat = heartbeat;
+		}
 	}
 }
