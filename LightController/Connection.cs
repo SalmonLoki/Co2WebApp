9c22a71f976de5ad768422631badf6487a4515d3
senya <kseniiakhromova@yandex.ru>
2019-05-28 07:15:35 +0300

    script

--- LightController/Connection.cs
+++ LightController/Connection.cs
@@ -2,15 +2,26 @@
 
 namespace LightController {
 	public class Connection {
-		private const string Url = "http://192.168.43.67/light/on";
+		private const string UrlLightON = "http://192.168.43.67/light/on";
+		private const string UrlLightOFF = "http://192.168.43.67/light/off";
+
+		private async void put(string Url) {
+			using (var client = new HttpClient()) {
+				using (var request = new HttpRequestMessage(HttpMethod.Put, Url)) {
+					try {
+						HttpResponseMessage unused = await client.SendAsync(request);
+					} catch (HttpRequestException ex) { }
+				}				
+			}	
+		}		
 
 		public async void sendSignalToLightConroller(int co2, double temperature) {
-			var client = new HttpClient();
-			var request = new HttpRequestMessage(HttpMethod.Get, Url);
-
-			try {
-				HttpResponseMessage unused = await client.SendAsync(request);
-			} catch (HttpRequestException ex) { }
+			if (co2 >= 900) {
+				put(UrlLightON);
+			}	
+			if (co2 < 900) {
+				put(UrlLightOFF);
+			}
 		}
 	}
 }
